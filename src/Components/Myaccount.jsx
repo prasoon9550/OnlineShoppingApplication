2f88f067eb927e7678e3cd8838d6cdda10285de8
jaya <jaya@vowerp.com>
2025-12-12 15:58:15 +0530

    file name fix

--- src/Components/Myaccount.jsx
+++ src/Components/Myaccount.jsx
@@ -1,5 +1,5 @@
     import React, { useState, useEffect } from "react";
-    import "./MyAccount.css";
+    import "./Myaccount.css";
     import { Link } from "react-router-dom";
 
     export default function MyAccount() {
